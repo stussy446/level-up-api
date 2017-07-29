5cfb9f509d5a53a64ae24a08e92e81bf62d4b313
Yun Jae Chang <yunjae0500@yahoo.com>
2017-07-29 16:20:56 -0700

    Update 20170729211614_create_friendships.rb

--- db/migrate/20170729211614_create_friendships.rb
+++ db/migrate/20170729211614_create_friendships.rb
@@ -1,8 +1,8 @@
 class CreateFriendships < ActiveRecord::Migration[5.1]
   def change
     create_table :friendships do |t|
-      t.integer :sender_id, null: false
-      t.integer :receiver_id, null: false
+      t.integer :user_id, null: false
+      t.integer :friend_id, null: false
       t.boolean :accepted, default: false 
 
       t.timestamps
