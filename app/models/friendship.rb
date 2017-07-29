a676591f685b6049fa0cd9539637a1bc232deca3
Yun Jae Chang <yunjae0500@yahoo.com>
2017-07-29 16:19:15 -0700

    Deleted inverse relationship

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,3 @@
 class Friendship < ApplicationRecord
-  belongs_to :recipient, class_name: "User", inverse_of: :received_requests
-  belongs_to :sender, class_name: "User", inverse_of: :sent_requests
+
 end
