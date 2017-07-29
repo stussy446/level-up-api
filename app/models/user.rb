eae117d9e97fe14fa4b3d9c263f44ed689fd8cc3
Yun Jae Chang <yunjae0500@yahoo.com>
2017-07-29 15:54:29 -0700

    Add friendship associations

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  validates :first_name, :last_name, :username, :email, :password, presence: true
+  validates :username, :email, uniqueness: true
+
+  has_many :friendships
+  has_many :received_friendships, class_name: "Friendship", foreign_key: "friend_id"
+
+  has_many :active_friends, -> { where(friendships: { accepted: true}) }, through: :friendships, source: :friend
+    has_many :received_friends, -> { where(friendships: { accepted: true}) }, through: :received_friendships, source: :user
+    has_many :pending_friends, -> { where(friendships: { accepted: false}) }, through: :friendships, source: :friend
+    has_many :requested_friendships, -> { where(friendships: { accepted: false}) }, through: :received_friendships, source: :user
+
+    def friends
+      active_friends | received_friends
+    end
+
+    def pending
+        pending_friends | requested_friendships
+    end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.delete_all
+
+User.create(first_name: "yun", last_name: "chang", username: "yunjae123", email: "yunjae0500@yahoo.com", password: "123")
