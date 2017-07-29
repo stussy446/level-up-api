1a71491ac9e1e37d243405577aab0521b5b2e047
Yun Jae Chang <yunjae0500@yahoo.com>
2017-07-29 16:19:34 -0700

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,5 @@ class User < ApplicationRecord
   validates :first_name, :last_name, :username, :email, :password, presence: true
   validates :username, :email, uniqueness: true
 
-  has_many :sent_requests, class_name: "Friendship", foreign_key: "sender_id"
-  has_many :received_requests, class_name: "Friendship", foreign_key: "receiver_id"
   has_many :achievements
 end
