e36974ad1b47365ba59cafcf740ddc3fbed98474
Jarrade Pierce <slim7@Jarrades-MacBook-Air.local>
2017-07-30 13:48:29 -0700

    added users api controller and association

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -6,10 +6,39 @@ class Api::UsersController < Api::ApiController
 
   def show
     @user = User.find_by(id: params[:id])
-    @user_achievements = @user.picked_achievements
-    render json: {
+    @user_achievements = @user.picked_achievements 
+    @completed_achievements = @user.completed_achievements
+    render status: 200, json: {
       user: @user, 
-      achievements: @user_achievements
-    }
+      achievements: @user.achievements,
+      users_achievements: @user_achievements,
+      completed_achievements: @completed_achievements
+    }.to_json
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      render status: 200, json:{
+        message: "Successfully created new user",
+        user: @user
+      }.to_json
+    else
+      render status: 422, json: {
+        message: "User creation failed",
+        user: @user.errors
+      }.json
+    end
+  end
+
+  private 
+
+  def user_params
+    params.permit(:username, :password)
+  end
+
 end
