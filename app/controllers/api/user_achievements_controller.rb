cdd72de5368ecc431734a16a436ee071736d8e2f
Steve Rector <steverector27@gmail.com>
2017-07-31 10:54:44 -0700

    Add achievement data to json render in user_achievement index

--- app/controllers/api/user_achievements_controller.rb
+++ app/controllers/api/user_achievements_controller.rb
@@ -2,7 +2,16 @@ class Api::UserAchievementsController < Api::ApiController
 
   def index 
     @user = User.find(1)
-    render json: @user
+    @user_achievements = @user.user_achievements.all
+    @achievement_data = @user_achievements.map do |user_achievement|
+      user_achievement.achievement
+    end
+
+    render json: {
+     user: @user,
+     user_achievements: @user_achievements,
+     achievement_data: @achievement_data
+    }.to_json
   end
 
 end
